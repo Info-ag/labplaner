41138fa1e9ce11dd6dff2cf20115bf8cd6e7a3c7
CeroProgramming <programming@quantcero.org>
2018-04-01 22:01:34 +0200

    Tested user part with database and added clear OOP

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 #pylint: disable-all
-from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Boolean, Date, and_
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Boolean, Date, exc, and_
 from configparser import ConfigParser
 from hashlib import sha256
 
@@ -7,27 +7,33 @@ class Database(object):
 
     def __init__(self):
 
+        self.config = Configuration()
         self.load_database()
-        self.load_table_users()
-        self.load_table_projectgroups()
 
     def load_database(self):
         '''Loads a MySQL/MariaDB database with the data in config.ini'''
 
-        self.database = create_engine('mysql://%s:%s@%s:%s/%s' % (Configuration().user, Configuration().password, Configuration().host, Configuration().port, Configuration().database))
+        self.database = create_engine('mysql://%s:%s@%s:%s/%s' % (self.config.user, self.config.password, self.config.host, self.config.port, self.config.database))
         self.database.echo = False
         self.metadata = MetaData(self.database)
 
-    def create_table_users(self):
+
+class User(Database):
+
+    def __init__(self):
+        super().__init__()
+        self.load()
+
+    def create(self):
         '''Creates the default table for user data.'''
 
         self.users = Table('users', self.metadata,
             Column('uid', Integer, primary_key=True),
-            Column('firstname', String),
-            Column('lastname', String),
-            Column('mail', String),
-            Column('password', String),
-            Column('birthday', Date),
+            Column('firstname', String(15)), #TODO Ideal String lengths
+            Column('lastname', String(15)),
+            Column('mail', String(35)),
+            Column('password', String(20)),
+            Column('birthday', String(10)),  #TODO Change this to Date ?
             Column('pgids', Integer),    #TODO Add multiple AG support
             Column('isadmin', Boolean),
             Column('ismentor', Boolean),
@@ -35,44 +41,65 @@ class Database(object):
             )
         self.users.create()
 
-    def load_table_users(self):
+    def load(self):
         '''Loads all user data.'''
 
-        self.users = Table('users', metadata, autoload=True)
+        try:
+            self.users = Table('users', self.metadata, autoload=True)
+        except exc.NoSuchTableError:
+            self.create()
 
-    def add_user(self, firstname, lastname, mail, password, birthday, pgids, ismentor, ismember, isadmin=False):
+
+    def add(self, firstname, lastname, mail, password, birthday, pgids, ismentor, ismember, isadmin=False):
         '''Creates a new user.'''
 
         insert = self.users.insert()
         insert.execute(firstname=firstname, lastname=lastname, mail=mail, password=password, birthday=birthday, pgids=pgids, isadmin=isadmin, ismentor=ismentor, ismember=ismember)
         return 'Added successfully %s %s' % (firstname, lastname)
 
-    def get_user_password(self, firstname, lastname):
+    def get_password(self, firstname, lastname):
         '''Search for the password of a given user.'''
 
-        select = self.users.select().where(_and(self.users.c.firstname == firstname, self.users.c.lastname == lastname))
+        select = self.users.select().where(and_(self.users.c.firstname == firstname, self.users.c.lastname == lastname))
         rowselected = select.execute()
         for row in rowselected:
-            return row.password #TODO The for loop is awkward cause I code it blind
+            return row.password
 
-    def create_table_projectgroups(self):
+
+class ProjectGroup(Database):
+
+    def __init__(self):
+        super().__init__()
+        self.load()
+
+    def create(self):
         '''Creates the default table for project group data.'''
 
         self.ags = Table('projectgroups', self.metadata,
             Column('pgid', Integer, primary_key=True),
-            Column('name', String),
-            Column('uids', String),     #TODO Add multiple User support
+            Column('name', String(20)),
+            Column('uids', String(30)),     #TODO Add multiple User support
             Column('mentoruids', Integer),
             Column('memberuids', Integer),
             )
         self.ags.create()
 
-    def load_table_projectgroups(self):
+    def load(self):
         '''Loads all project group data.'''
 
-        self.ags = Table('projectgroups', metadata, autoload=True)
+        try:
+            self.ags = Table('projectgroups', self.metadata, autoload=True)
+        except exc.NoSuchTableError:
+            self.create()
 
-    def create_table_dates(self):
+
+class Dates(Database):
+
+    def __init__(self):
+        super().__init__()
+        self.load()
+
+    def create(self):
         '''Creates the default table for project group data.'''
 
         self.dates = Table('dates', self.metadata,
@@ -80,12 +107,15 @@ class Database(object):
             )
         self.dates.create()
 
-    def load_table_dates(self):
+    def load(self):
         '''Loads all dates data.'''
 
-        self.dates = Table('dates', metadata, autoload=True)
+        try:
+            self.dates = Table('dates', self.metadata, autoload=True)
+        except exc.NoSuchTableError:
+            self.create()
 
-    def add_column_table_dates(self):
+    def add_column(self):
         '''Add column to dates data.'''
 
         column = Column('new column', String(100), primary_key=True)
@@ -97,28 +127,59 @@ class Database(object):
 class Configuration(object):
 
     def __init__(self):
+        self.config_file = 'config.ini'
         self.config = ConfigParser()
-        self.config.read('config.ini')
-        self.user = self.duser()
-        self.password = self.dpassword()
-        self.database = self.ddatabase()
-        self.host = self.dhost()
-        self.port = self.dport()
+        self.config.read(self.config_file)
 
-    def duser(self):
+    @property
+    def user(self):
         return self.config['Credentials']['User']
 
-    def dpassword(self):
+    @user.setter
+    def user(self, value):
+        self.config['Credentials']['User'] = value
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
+
+    @property
+    def password(self):
         return str(self.config['Credentials']['Password'])
 
-    def ddatabase(self):
+    @password.setter
+    def password(self, value):
+        self.config['Credentials']['Password'] = value
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
+
+    @property
+    def database(self):
         return self.config['Credentials']['Database']
 
-    def dhost(self):
+    @database.setter
+    def database(self, value):
+        self.config['Credentials']['Database'] = value
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
+
+    @property
+    def host(self):
         return self.config['Credentials']['Host']
 
-    def dport(self):
+    @host.setter
+    def host(self, value):
+        self.config['Credentials']['Host'] = value
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
+
+    @property
+    def port(self):
         return int(self.config['Credentials']['Port'])
+
+    @port.setter
+    def port(self, value):
+        self.config['Credentials']['Port'] = value
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
 
 
 if __name__ == '__main__':
