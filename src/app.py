176f24b9a178a73b3741009ccb42d392715848b4
Jonas Drotleff <jonas.drotleff@gmail.com>
2018-04-17 00:00:14 +0200

    Deprecate main.py
    Remove `main.py` as it seems redundant. Instead we just use `app.py`.
    This helps us to make clear what the entry point is. It's important for
    the db, models and resources (api).

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 # pylint: disable-all
-from flask import Flask, \
-    render_template, \
+from flask import render_template, \
     make_response, \
     redirect, \
     request, \
@@ -14,9 +13,10 @@ from flask_migrate import Migrate
 from bcrypt import checkpw
 
 from database import User
+import dbconfig
 
 app = Flask(__name__)
-app.config.from_object(config.Config)
+app.config.from_object(dbconfig.DBConfig)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
@@ -26,107 +26,34 @@ USERDB = {'Simon': {'pw': b'$2b$12$dFZFqlDLXkPJyqRnTzPGhu6TqxkaWtiQ17WdE5P9o0ctF
                     'userid': '7ed586770508da0ffac6d19fe99750b083a1fe84235574f4d4c48f1f8d6f241a'}}  # sha256(bytes('Simon' + '1', 'utf-8')).hexdigest() #hashpw(bytes('123456', 'UTF-8'), gensalt())
 
 
-class FlaskSites(object):
+@app.route('/')
+def index(text=''):
+    return render_template('index.html', message=text)
 
-    def __init__(self):
-        pass
 
-    @staticmethod
-    @app.route('/')
-    def index(text=''):
-        return render_template('index.html', message=text)
+@app.route('/login/')
+def login():
+    return render_template('login.html', warning='')
 
-    @staticmethod
-    @app.route('/login/')
-    def login():
-        return render_template('login.html', warning='')
 
-    @staticmethod
-    @app.route('/logout/', methods=['POST', 'GET'])
-    def logout():
-        resp = make_response(render_template('logout.html'))
-        resp.set_cookie('userID', '')
-        resp.set_cookie('user', '')
-        return resp
+@app.route('/logout/', methods=['POST', 'GET'])
+def logout():
+    resp = make_response(render_template('logout.html'))
+    resp.set_cookie('userID', '')
+    resp.set_cookie('user', '')
+    return resp
 
-    @staticmethod
-    @app.route('/dashboard/', methods=['POST', 'GET'])
-    def dashboard():
-        if request.method == 'POST':
 
-            user = request.form['name']
-            password = request.form['password']
-            try:
-                if checkpw(bytes(password, 'UTF-8'), USERDB[user]['pw']):
-                    userid = USERDB[user]['userid']
+@app.route('/dashboard/', methods=['POST', 'GET'])
+def dashboard():
+    if request.method == 'POST':
 
-                    resp = make_response(render_template('dashboard.html', name=user))
-                    resp.set_cookie('userID', userid)
-                    resp.set_cookie('user', user)
-                    return resp
-            except KeyError:
-                pass
-            return render_template('login.html', warning='Username or password wrong..')
-        else:
-            if request.cookies.get('user'):
-                if request.cookies.get('user') != '':
-                    return render_template('dashboard.html', name=request.cookies.get('user'))
-            return redirect(url_for('login'))
-
-    @staticmethod
-    @app.route('/planer/<name>/')
-    def planer():
-        return render_template('login.html', warning='Something went wrong')
-
-    @staticmethod
-    @app.route('/api/create/user', methods=['POST'])
-    def create_user():
-        data = request.data
-        users = User()
-        users.add(firstname=data.get('lastname'), lastname=data.get('lastname'), mail=data.get('mail'),
-                  password=data.get('password'), birthday=data.get('birthday'), pgids=data.get('pgids'),
-                  ismentor=data.get('ismentor'), ismember=data.get('ismentor'), isadmin=data.get('isadmin'))
-
-    @staticmethod
-    @app.route('/api/user/<uid>/', methods=['GET', 'POST'])
-    def user_by_uid(uid):
-        '''Returns all data of a user.'''
-        client_id = request.args.get('id')
-        secret_key = request.args.get('key')
-        if not client_id or not secret_key:  # TODO insert API validation check
-            return abort(401)
-
-        if request.method == "GET":
-            user = User().get(uid=uid)
-            if not user:
-                return abort(404)
-
-            response = jsonify({"uid": user.uid,
-                                "firstname": user.firstname,
-                                "lastname": user.lastname,
-                                "mail": user.mail,
-                                "birthday": user.birthday,
-                                "pgids": user.pgids,
-                                "isadmin": user.isadmin,
-                                "ismentor": user.ismentor,
-                                "ismember": user.ismember})
-            response.status_code = 201
-            return response
-        elif request.method == "DELETE":
-            users = User()
-            users.delete(uid=uid)
-
-    @staticmethod
-    @app.route('/api/auth', method='POST')
-    def dashboard():
         user = request.form['name']
         password = request.form['password']
-
-        user = User()
-        user = user.get(mail=user)
         try:
-            if checkpw(bytes(password, 'UTF-8'), user.password):
+            if checkpw(bytes(password, 'UTF-8'), USERDB[user]['pw']):
                 userid = USERDB[user]['userid']
+
                 resp = make_response(render_template('dashboard.html', name=user))
                 resp.set_cookie('userID', userid)
                 resp.set_cookie('user', user)
@@ -134,69 +61,140 @@ class FlaskSites(object):
         except KeyError:
             pass
         return render_template('login.html', warning='Username or password wrong..')
+    else:
+        if request.cookies.get('user'):
+            if request.cookies.get('user') != '':
+                return render_template('dashboard.html', name=request.cookies.get('user'))
+        return redirect(url_for('login'))
 
-    @staticmethod
-    @app.route('/api/user/<uid>/<attribute>/', methods=['GET'])
-    def user_attribute_by_uid(uid, attribute):
-        '''Returns a specified value of a user.'''
-        aid = request.args.get('api')
-        if not aid:  # TODO insert API validation check
-            return abort(401)
 
-        if request.method == "GET":
-            user = User().get(uid=uid)
-            if not user:
-                return abort(404)
+@app.route('/planer/<name>/')
+def planer():
+    return render_template('login.html', warning='Something went wrong')
 
-            value = user[attribute]
-            if not value:
-                return abort(404)
 
-            response = jsonify(value)
-            response.status_code = 201
-            return response
+@app.route('/api/create/user', methods=['POST'])
+def create_user():
+    data = request.data
+    users = User()
+    users.add(firstname=data.get('lastname'), lastname=data.get('lastname'), mail=data.get('mail'),
+              password=data.get('password'), birthday=data.get('birthday'), pgids=data.get('pgids'),
+              ismentor=data.get('ismentor'), ismember=data.get('ismentor'), isadmin=data.get('isadmin'))
 
-    @staticmethod
-    @app.route('/api/user/<uid>/<attribute>/<patch>', methods=['PATCH'])
-    def user_atrribute_by_uid(uid, attribute, patch):
-        '''Returns a specified value of a user.'''
-        aid = request.args.get('api')
-        if not aid:  # TODO insert API validation check
-            return abort(401)
 
-        if request.method == "PATCH":
-            users = User()
+@app.route('/api/user/<uid>/', methods=['GET', 'POST'])
+def user_by_uid(uid):
+    '''Returns all data of a user.'''
+    client_id = request.args.get('id')
+    secret_key = request.args.get('key')
+    if not client_id or not secret_key:  # TODO insert API validation check
+        return abort(401)
 
-            users.patch(uid=uid, name=attribute, value=patch)
-            response = jsonify({"success": True})
-            response.status_code = 201
-            return response
+    if request.method == "GET":
+        user = User().get(uid=uid)
+        if not user:
+            return abort(404)
 
-    @staticmethod
-    @app.route('/api/users/', methods=['GET'])
-    def users():
-        '''Returns all datas of all users.'''
-        aid = request.args.get('api')
-        if not aid:  # TODO insert API validation check
-            return abort(401)
+        response = jsonify({"uid": user.uid,
+                            "firstname": user.firstname,
+                            "lastname": user.lastname,
+                            "mail": user.mail,
+                            "birthday": user.birthday,
+                            "pgids": user.pgids,
+                            "isadmin": user.isadmin,
+                            "ismentor": user.ismentor,
+                            "ismember": user.ismember})
+        response.status_code = 201
+        return response
+    elif request.method == "DELETE":
+        users = User()
+        users.delete(uid=uid)
 
-        if request.method == "GET":
-            all_users = User().get_all()
-            if not all_users:
-                return abort(404)
 
-            response_list = []
-            for user in all_users:
-                response_list.append({"uid": user.uid,
-                                      "firstname": user.firstname,
-                                      "lastname": user.lastname,
-                                      "mail": user.mail,
-                                      "birthday": user.birthday,
-                                      "pgids": user.pgids,
-                                      "isadmin": user.isadmin,
-                                      "ismentor": user.ismentor,
-                                      "ismember": user.ismember})
+@app.route('/api/auth', method='POST')
+def dashboard():
+    user = request.form['name']
+    password = request.form['password']
 
-            response = jsonify(response_list)
-            response.status_code = 201
-            return response
+    user = User()
+    user = user.get(mail=user)
+    try:
+        if checkpw(bytes(password, 'UTF-8'), user.password):
+            userid = USERDB[user]['userid']
+            resp = make_response(render_template('dashboard.html', name=user))
+            resp.set_cookie('userID', userid)
+            resp.set_cookie('user', user)
+            return resp
+    except KeyError:
+        pass
+    return render_template('login.html', warning='Username or password wrong..')
+
+
+@app.route('/api/user/<uid>/<attribute>/', methods=['GET'])
+def user_attribute_by_uid(uid, attribute):
+    '''Returns a specified value of a user.'''
+    aid = request.args.get('api')
+    if not aid:  # TODO insert API validation check
+        return abort(401)
+
+    if request.method == "GET":
+        user = User().get(uid=uid)
+        if not user:
+            return abort(404)
+
+        value = user[attribute]
+        if not value:
+            return abort(404)
+
+        response = jsonify(value)
+        response.status_code = 201
+        return response
+
+
+@app.route('/api/user/<uid>/<attribute>/<patch>', methods=['PATCH'])
+def user_atrribute_by_uid(uid, attribute, patch):
+    '''Returns a specified value of a user.'''
+    aid = request.args.get('api')
+    if not aid:  # TODO insert API validation check
+        return abort(401)
+
+    if request.method == "PATCH":
+        users = User()
+
+        users.patch(uid=uid, name=attribute, value=patch)
+        response = jsonify({"success": True})
+        response.status_code = 201
+        return response
+
+
+@app.route('/api/users/', methods=['GET'])
+def users():
+    '''Returns all datas of all users.'''
+    aid = request.args.get('api')
+    if not aid:  # TODO insert API validation check
+        return abort(401)
+
+    if request.method == "GET":
+        all_users = User().get_all()
+        if not all_users:
+            return abort(404)
+
+        response_list = []
+        for user in all_users:
+            response_list.append({"uid": user.uid,
+                                  "firstname": user.firstname,
+                                  "lastname": user.lastname,
+                                  "mail": user.mail,
+                                  "birthday": user.birthday,
+                                  "pgids": user.pgids,
+                                  "isadmin": user.isadmin,
+                                  "ismentor": user.ismentor,
+                                  "ismember": user.ismember})
+
+        response = jsonify(response_list)
+        response.status_code = 201
+        return response
+
+
+if __name__ == '__main__':
+    app.run()
--- src/dbconfig.py
+++ src/dbconfig.py
@@ -3,8 +3,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config(object):
-    # ...
+class DBConfig(object):
+    # TODO change to mysql database
+    # Read SQLAlchemy docs
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
