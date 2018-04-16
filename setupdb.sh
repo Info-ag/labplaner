b351b7ffc4e75721f9b312aa9a920865a4912fc1
Jonas Drotleff <jonas.drotelff@gmail.com>
2018-04-16 22:10:49 +0200

    Adding head
    Signed-off-by: Jonas Drotleff <jonas.drotleff@gmail.com>

--- setupdb.sh
+++ setupdb.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+###########################################
+# Script used to setup the local database.
+# You might have to run this as root.
+###########################################
+
+
 mysql -e "CREATE USER 'test'@'localhost' IDENTIFIED BY '123456';";
 mysql -e "CREATE DATABASE base CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
 mysql -e "GRANT ALL PRIVILEGES ON base.* TO 'test'@'localhost';";
