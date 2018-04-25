37261122dac34d208f1a8f2f487cf8dca33534c1
OleKet <olekettern@gmail.com>
2018-04-25 16:36:54 +0200

    Move Scripts in external files

--- src/static/js/pages/login.js
+++ src/static/js/pages/login.js
@@ -1,1 +1,46 @@
+var signUpForm = $("#logInForm");
+    var formMessages = $("#formMessages");
+    $(signUpForm).submit(function (event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+        if ($("#password").val() == $("#password_rpt").val()) {
+            var formData = $(signUpForm).serialize();
+            console.log(formData);
+            $.ajax({
+                type: 'POST',
+                url: $(signUpForm).attr('action'),
+                data: formData,
+                dataType: "json",
+                cache: false,
+                beforeSend: function () {
+                    $("#signUpFormFieldset").prop('disabled', true);
+                    formMessages.text("Anfrage wurde gesendet, es wird auf eine Antwort gewartet.");
+                    $("#signup").addClass("loading");
+                    return true;
+                },
+                statusCode: function () {
+
+                }
+            }).done(function (response) {
+                $("#signup").removeClass("loading");
+                $.ajax({
+                    type: 'POST',
+                    url: '{{ url_for('auth.login') }}',
+                    data: formData,
+                    dataType: "json",
+                    cache: false
+                }).done(function (response) {
+                    window.location.href = response.redirect;
+                })
+            }).fail(function (data) {
+                $("#signup").removeClass("loading");
+                $("#signUpFormFieldset").prop('disabled', false);
+                formMessages.text("Irgendetwas lief falsch");
+            });
+        } else {
+            alert("Passwort stimmt nicht überein");
+            //Visualisieren statt dem Alert
+        }
+
+    });
 
