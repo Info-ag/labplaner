c4176c9436f2ea30c3b9add4b9f2decaa8c33f38
OleKet <olekettern@gmail.com>
2018-04-27 12:12:46 +0200

    Added Route for invite

--- src/blueprints/ag.py
+++ src/blueprints/ag.py
@@ -5,9 +5,18 @@ from app import db
 bp = Blueprint("ag", __name__)
 
 
+
 @bp.route("/add", methods=["GET"])
 def create_ag():
     if not g.session.authenticated:
         return redirect(url_for("auth.login"))
 
     return render_template('ag/add.html')
+
+
+@bp.route("/invite", methods=["GET"])
+def create_ag():
+    if not g.session.authenticated:
+        return redirect(url_for("auth.login"))
+
+    return render_template('ag/invite.html')
