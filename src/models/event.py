1f9a1a12120d047cb0241145730a5ebe291d30d6
Jonas Drotleff <jonas.drotleff@gmail.com>
2018-04-29 19:00:13 +0200

    not unique

--- src/models/event.py
+++ src/models/event.py
@@ -8,7 +8,7 @@ class Event(db.Model):
     __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    display_name = db.Column(db.String(48), unique=True, nullable=False)
+    display_name = db.Column(db.String(48), nullable=False)
     description = db.Column(db.String(280), nullable=False)
     date = db.Column(db.Date, nullable=True)
     ag = db.Column(db.Integer, db.ForeignKey("ags.id"))
