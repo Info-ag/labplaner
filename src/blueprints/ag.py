07fa69744e0684f6eed1584fb397bcc31edfcad4
OleKet <olekettern@gmail.com>
2018-06-11 10:18:23 +0200

    added multiple utils for ag blueprints

--- src/blueprints/ag.py
+++ src/blueprints/ag.py
@@ -4,7 +4,7 @@ from werkzeug.exceptions import NotFound, Unauthorized
 from src.models.associations import UserAG
 from src.models.ag import AG, AGSchema, AGSchemaIntern
 from src.main import db
-from src.utils import requires_auth, requires_mentor
+from src.utils import requires_auth, requires_mentor, requires_membership
 
 from config.regex import ag_regex
 
@@ -23,67 +23,28 @@ def create_ag():
 
 
 @bp.route('/<ag_name>', methods=['GET'])
-@requires_auth()
-def ag_dashboard(ag_name):
-    if db.session.query(exists().where(AG.name == ag_name)).scalar():
-        ag: AG = AG.query.filter_by(name=ag_name).scalar()
-        if db.session.query(exists().where(UserAG.user_id == g.session.user_id and UserAG.ag_id == ag.id)).scalar():
-            user_ag = UserAG.query.filter_by(user_id=g.session.user_id, ag_id=ag.id).scalar()
-            if user_ag.role != 'NONE':
-                schema = AGSchemaIntern()
-                schema.context = {'ag_id': ag.id}
-                return render_template('ag/dashboard.html', my_role='user_ag.role', ag=schema.dump(ag),
-                                       title=ag.display_name)
-
-        return Unauthorized()
-
-    else:
-        return NotFound()
+@requires_membership()
+def ag_dashboard(ag_name, ag, user_ag):
+    schema = AGSchemaIntern()
+    schema.context = {'ag_id': ag.id}
+    return render_template('ag/dashboard.html', my_role=user_ag.role, ag=schema.dump(ag),
+                            title=ag.display_name)
 
 
 @bp.route('/<ag_name>/invite', methods=['GET'])
-@requires_auth()
-def invite_ag(ag_name):
-
-    if db.session.query(exists().where(AG.name == ag_name)).scalar():
-        ag: AG = AG.query.filter_by(name=ag_name).scalar()
-        if db.session.query(exists().where(UserAG.user_id == g.session.user_id and UserAG.ag_id == ag.id)).scalar():
-            user_ag = UserAG.query.filter_by(user_id=g.session.user_id, ag_id=ag.id).scalar()
-            if user_ag.role == 'MENTOR':
-                return render_template('ag/invite.html', ag=ag_schema_intern.dump(ag),
-                                       title=f'Invite {ag.display_name}')
-        return Unauthorized()
-    else:
-        return NotFound()
+@requires_mentor()
+def invite_ag(ag_name, ag, user_ag):
+    return render_template('ag/invite.html', ag=ag_schema_intern.dump(ag), title=f'Invite {ag.display_name}')
 
 
 # Events
 
 @bp.route('/<ag_name>/event/add', methods=['GET'])
-@requires_auth()
-def create_event(ag_name):
-
-    if db.session.query(exists().where(AG.name == ag_name)).scalar():
-        ag: AG = AG.query.filter_by(name=ag_name).scalar()
-        if db.session.query(exists().where(UserAG.user_id == g.session.user_id and UserAG.ag_id == ag.id)).scalar():
-            user_ag = UserAG.query.filter_by(user_id=g.session.user_id, ag_id=ag.id).scalar()
-            if user_ag.role == 'MENTOR':
-                return render_template('ag/event/add.html', ag=ag_schema_intern.dump(ag),
-                                       title=f'New Event {ag.display_name}')
-
-        return Unauthorized()
-
-    return NotFound()
+@requires_mentor()
+def create_event(ag_name, ag, user_ag):
+    return render_template('ag/event/add.html', ag=ag_schema_intern.dump(ag), title=f'New Event {ag.display_name}')
 
 @bp.route('/<ag_name>/settings', methods=['GET'])
-@requires_auth()
-def ag_settings(ag_name):
-    if db.session.query(exists().where(AG.name == ag_name)).scalar():
-        ag: AG = AG.query.filter_by(name=ag_name).scalar()
-        if db.session.query(exists().where(UserAG.user_id == g.session.user_id and UserAG.ag_id == ag.id)).scalar():
-            user_ag = UserAG.query.filter_by(user_id=g.session.user_id, ag_id=ag.id).scalar()
-            if user_ag.role == 'MENTOR':
-                return render_template('ag/settings.html', title='Create AG')
-        return Unauthorized()
-    else:
-        return NotFound()
+@requires_mentor()
+def ag_settings(ag_name, ag, user_ag):
+    return render_template('ag/settings.html', title='Create AG')
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,13 @@
 from functools import wraps
 
 from flask import g, redirect, url_for, request
-from werkzeug.exceptions import Unauthorized
+from sqlalchemy import exists
+from src.main import db
+from src.models.ag import AG
+from src.models.associations import UserAG
+
+from werkzeug.exceptions import Unauthorized, NotFound
+import inspect
 
 
 def requires_auth():
@@ -21,4 +27,72 @@ def after_this_request(f):
         g.after_request_callbacks = []
     g.after_request_callbacks.append(f)
     return f
-  
+
+def requires_existing_ag():
+    def wrapper(f):
+        @wraps(f)
+        @requires_auth()
+        def wrapped(*args, **kwargs):
+            ag_name = kwargs.get('ag_name', None)
+            if db.session.query(exists().where(AG.name == ag_name)).scalar() and ag_name is not None:
+                ag: AG = AG.query.filter_by(name=ag_name).scalar()
+                kwargs.setdefault('ag', ag)
+                return f(*args, **kwargs)
+            else:
+                return NotFound()
+        return wrapped
+    return wrapper
+
+def requires_ag():
+    def wrapper(f):
+        @wraps(f)
+        @requires_existing_ag()
+        def wrapped(*args, **kwargs):
+            ag_name = kwargs.get('ag_name', None)
+            ag: AG = AG.query.filter_by(name=ag_name).scalar()
+            kwargs.setdefault('ag', ag)
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
+
+def requires_member():
+    def wrapper(f):
+        @wraps(f)
+        @requires_ag()
+        def wrapped(*args, **kwargs):
+            ag = kwargs.get('ag')
+            if db.session.query(exists().where(UserAG.user_id == g.session.user_id and UserAG.ag_id == ag.id)).scalar():
+                return f(*args, **kwargs)
+            else:
+                return Unauthorized()
+        return wrapped
+    return wrapper
+
+def requires_membership():
+    def wrapper(f):
+        @wraps(f)
+        @requires_member()
+        def wrapped(*args, **kwargs):
+            ag = kwargs.get('ag')
+            user_ag = UserAG.query.filter_by(user_id=g.session.user_id, ag_id=ag.id).scalar()
+            if(user_ag.role != "NONE"):
+                kwargs.setdefault('user_ag', user_ag)
+                return f(*args, **kwargs)
+            else:
+                return Unauthorized()
+        return wrapped
+    return wrapper
+
+def requires_mentor():
+    def wrapper(f):
+        @wraps(f)
+        @requires_membership()
+        def wrapped(*args, **kwargs):
+            user_ag = kwargs.get('user_ag')
+            if user_ag.role == 'MENTOR':
+                return f(*args, **kwargs)
+            else:
+                return Unauthorized()
+        return wrapped
+    return wrapper
+
