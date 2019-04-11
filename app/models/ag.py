ef0562d2480b2dc451d5e586cf1110b42b6f6486
Jonas Drotleff <jonas.drotleff@gmail.com>
2019-04-11 11:53:09 +0200

    fix ag model

--- app/models/ag.py
+++ app/models/ag.py
@@ -1,107 +1,132 @@
-'''
-Database models regardning AG --> AG, AGMEssage (,Message)
+"""Database models regardning AG --> AG, AGMEssage (,Message)
 Marshmallow Schema regarding those database models --> AGSchema, AGSchemaIntern, AGMessageSchema
-'''
+"""
 
-from datetime import datetime
 from sqlalchemy import and_
 from flask import g
-from app import ma
+from marshmallow import Schema, Nested, Method
+
 from app.models import db
 from app.models.associations import UserAG, UserAGMessage
 
 
+__all__ = ['AG', 'AGSchema', 'AGSchemaIntern']
+
+
 class AG(db.Model):
-    __tablename__ = 'ags'
+    """AGs are working groups
+
+    Working groups have a number of users related to them. They can have
+    different rules such as mentors or students.
+
+    Relationships:
+        - `ags_users`
+        - `ags_events`
+        - `messages`
+    """
+    __tablename__ = 'ag'
+
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    name = db.Column(db.String(16), unique=True, nullable=False)
-    color = db.Column(db.String())
-    # enum: primary, success, dark, warning, error
-    display_name = db.Column(db.String(48), unique=True, nullable=False)
-    description = db.Column(db.String(140), nullable=False)
+    name = db.Column(db.String(length=16), unique=True, nullable=False)
+    display_name = db.Column(db.String(length=48), unique=True, nullable=False)
+    description = db.Column(db.Text(length=140), nullable=True)
 
-    users = db.relationship('User', secondary='users_ags', order_by='UserAG.role')
+    # Always use set_color and get_hex_color
+    color = db.Column(db.LargeBinary(length=24))
 
-    actual_users = db.relationship('User',\
-                                   secondary='users_ags',\
-                                   primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role != "NONE")',\
-                                   viewonly=True)
-    invited_users = db.relationship('User',
-                                    secondary='users_ags',\
-                                    primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role == "NONE", UserAG.status == "INVITED")',\
-                                    viewonly=True)
-    applied_users = db.relationship('User',\
-                                    secondary='users_ags',\
-                                    primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role == "NONE", UserAG.status == "APPLIED")',\
-                                    viewonly=True)
-    mentors = db.relationship('User',\
-                              secondary='users_ags',\
-                              primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role == "MENTOR", UserAG.status == "ACTIVE")',\
-                              viewonly=True)
+    users = db.relationship('User', secondary='ags_users',
+                            order_by='UserAG.role')
 
-    events = db.relationship('Event')
+    actual_users = db.relationship(
+        'User',
+        secondary='ags_users',
+        primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role != "NONE")',
+        viewonly=True)
+    invited_users = db.relationship(
+        'User',
+        secondary='ags_users',
+        primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role == "NONE", UserAG.status == "INVITED")',
+        viewonly=True)
+    applied_users = db.relationship(
+        'User',
+        secondary='ags_users',
+        primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role == "NONE", UserAG.status == "APPLIED")',
+        viewonly=True)
+    mentors = db.relationship(
+        'User',
+        secondary='ags_users',
+        primaryjoin='and_(User.id == UserAG.user_id, AG.id == UserAG.ag_id, UserAG.role == "MENTOR", UserAG.status == "ACTIVE")',
+        viewonly=True)
 
-    messages = db.relationship('AGMessage')
+    events = db.relationship('Event', secondary='ags_events')
+    messages = db.relationship('Message')
 
-    unread_messages = db.relationship('AGMessage',\
-                                    secondary='users_ag_messages',\
-                                    primaryjoin='and_(UserAGMessage.message_id == AGMessage.id, AG.id == AGMessage.ag_id, UserAGMessage.read == 0, UserAGMessage.user_id == 1)',\
-                                    viewonly=True)
+    def get_hex_color(self) -> str:
+        """Hex color from binary
 
-    read_messages = db.relationship('AGMessage',\
-                                    secondary=f'users_ag_messages',\
-                                    primaryjoin=f'and_(UserAGMessage.message_id == AGMessage.id, AG.id == AGMessage.ag_id, UserAGMessage.read == 1, UserAGMessage.user_id == 1)',\
-                                    viewonly=True)
+        Takes the binary color and create valid hexadecimal color string
+        including '#' (e.g. #FFFFFF for white)
+        """
+        return f'#{self.color.hex()}'
+
+    def set_color(self, color: str):
+        """Set binary color from hex color string
+        Example: ag.set_color('#99FF23')
+        
+        :param color: hexadecimal color string (such as produced by 
+        AG.get_hex_color). May include '#'
+        :raises ValueError: color is not hex (excluding #)
+        """
+        self.color = bytes.fromhex(color.replace('#', ''))
 
     def __repr__(self):
         return f'<AG {self.name}>'
 
 
-class AGMessage(db.Model):
-    __tablename__ = 'ag_messages'
-    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    ag_id = db.Column(db.Integer, db.ForeignKey('ags.id'))
-    subject = db.Column(db.String(20))
-    message = db.Column(db.String(1000))
-    created = db.Column(db.DateTime, default=datetime.now())
-
-    users = db.relationship('UserAGMessage', secondary='users',\
-                            primaryjoin='and_(AGMessage.id == UserAGMessage.message_id, UserAGMessage.user_id == User.id)', viewonly=True)
-
-    def __repr__(self):
-        return f'<AGMessage {self.id}>'
-
-class AGSchema(ma.Schema):
-    users = ma.Nested('UserSchema', many=True, exclude=('ags',))
-    events = ma.Nested('EventSchema', many=True, exclude=('ag',))
-    association = ma.Method('get_association', many=True)
+class AGSchema(Schema):
+    users = Nested('UserSchema', many=True, exclude=('ags',))
+    events = Nested('EventSchema', many=True, exclude=('ag',))
+    association = Method('get_association', many=True)
 
     def get_association(self, obj: AG):
         if not g.session.authenticated:
             return None
         ag_id = obj.id
-        user_ag = UserAG.query.filter_by(user_id=g.user.id, ag_id=ag_id).scalar()
+        user_ag = UserAG.query.filter_by(user_id=g.user.id,
+                                         ag_id=ag_id).scalar()
         return {'role': user_ag.role, 'status': user_ag.status}
 
     class Meta:
-        fields = ('id', 'name', 'display_name', 'description', 'color', 'association')
+        fields = (
+            'id',
+            'name',
+            'display_name',
+            'description',
+            'color',
+            'association',
+        )
 
 
-class AGSchemaIntern(ma.Schema):
-    users = ma.Nested('UserSchema', many=True, exclude=('ags',))
-    events = ma.Nested('EventSchema', many=True, exclude=('ag',))
-    actual_users = ma.Nested('UserSchema', many=True, exclude=('ags',))
-    invited_users = ma.Nested('UserSchema', many=True, exclude=('ags',))
-    applied_users = ma.Nested('UserSchema', many=True, exclude=('ags',))
-    messages = ma.Nested('AGMessageSchema', many=True)
-    read_messages = ma.Method('get_read_messages')
-    unread_messages = ma.Method('get_unread_messages')
+class AGSchemaIntern(Schema):
+    users = Nested('UserSchema', many=True, exclude=('ags',))
+    events = Nested('EventSchema', many=True, exclude=('ag',))
+    actual_users = Nested('UserSchema', many=True, exclude=('ags',))
+    invited_users = Nested('UserSchema', many=True, exclude=('ags',))
+    applied_users = Nested('UserSchema', many=True, exclude=('ags',))
+    messages = Nested('AGMessageSchema', many=True)
+
+    read_messages = Method('get_read_messages')
+    unread_messages = Method('get_unread_messages')
 
     def get_unread_messages(self, obj: AG):
         if not g.session.authenticated:
             return None
         ag_id = obj.id
-        ag_message = db.session.query(AGMessage).join(UserAGMessage, and_(AGMessage.ag_id == ag_id, UserAGMessage.user_id == g.session.user_id, UserAGMessage.message_id == AGMessage.id, UserAGMessage.read == 0)).all()
+        ag_message = db.session.query(AGMessage).join(UserAGMessage, and_(
+            AGMessage.ag_id == ag_id,
+            UserAGMessage.user_id == g.session.user_id,
+            UserAGMessage.message_id == AGMessage.id,
+            UserAGMessage.read == 0)).all()
         ag_messages_Schema = AGMessageSchema(many=True)
         return ag_messages_Schema.dump(ag_message)
 
@@ -109,46 +134,28 @@ class AGSchemaIntern(ma.Schema):
         if not g.session.authenticated:
             return None
         ag_id = obj.id
-        ag_message = db.session.query(AGMessage).join(UserAGMessage, and_(AGMessage.ag_id == ag_id, UserAGMessage.user_id == g.session.user_id, UserAGMessage.message_id == AGMessage.id, UserAGMessage.read == 1)).all()
+        ag_message = db.session.query(AGMessage).join(UserAGMessage, and_(
+            AGMessage.ag_id == ag_id,
+            UserAGMessage.user_id == g.session.user_id,
+            UserAGMessage.message_id == AGMessage.id,
+            UserAGMessage.read == 1)).all()
         if len(ag_message) > 1:
             ag_messages_Schema = AGMessageSchema(many=True, exclude=('users',))
         else:
             ag_messages_Schema = AGMessageSchema(many=True, exclude=('users',))
         return ag_messages_Schema.dump(ag_message)
 
-
     class Meta:
         fields = (
-            'id', 'name', 'display_name', 'description', 'users', 'events', 'color', 'actual_users', 'invited_users',
-            'applied_users', 'messages', 'unread_messages', 'read_messages')
-
-
-
-
-class AGMessageSchema(ma.Schema):
-    read = ma.Method('get_read_status')
-    users = ma.Nested('UserAGMessageSchema', many=True)
-
-
-    def get_read_status(self, obj: AGMessage):
-        if not g.session.authenticated:
-            return None
-        user_ag_message = db.session.query(UserAGMessage).filter_by(user_id=g.session.user_id, message_id=obj.id).scalar()
-        return user_ag_message.read
-
-
-    class Meta:
-        fields = (
-            'id', 'ag_id', 'subject', 'message', 'read', 'created', 'users'
+            'id',
+            'name',
+            'display_name',
+            'description',
+            'users',
+            'events',
+            'color',
+            'actual_users',
+            'invited_users',
+            'applied_users',
+            'messages',
         )
-
-class Message(db.Model):
-    __tablename__ = 'messages'
-    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    subject = db.Column(db.String(20))
-    message = db.Column(db.String(1000))
-    created = db.Column(db.DateTime, default=datetime.now())
-
-
-    def __repr__(self):
-        return f'<Message {self.id}>'
