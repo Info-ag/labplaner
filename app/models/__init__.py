31d54ecd81667456dc6bd1dbfe34394779675c25
Jonas Drotleff <jonas.drotleff@gmail.com>
2019-04-11 11:52:31 +0200

    clean up comments

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,10 @@
-'''
-db instance
-'''
+"""Models
+
+This package contains all application models and schemes used for
+database access and serialization.
+We use SQLAlchemy and Marshmallow for our models and schemes.
+"""
 from flask_sqlalchemy import SQLAlchemy
 
-
-# initialize db instance
-db = SQLAlchemy()
+# App instance is set later in `app.create_app`
+db = SQLAlchemy(session_options={"autoflush": False})
