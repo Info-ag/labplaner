cacb5d4c938c356e91a2e1bc943ad960ce70ed30
Jonas Drotleff <jonas.drotleff@gmail.com>
2019-04-11 11:43:15 +0200

    add i18n

---
+++ app/i18n/__init__.py
@@ -0,0 +1,8 @@
+from flask_babel import Babel
+#from flask_moment import Moment
+
+# Moment and Babel are used for internationalisation (i18n)
+# Babel is used to translate the app using pot files
+# Moment is used for internationalized date/time formats
+babel = Babel()
+#moment = Moment()
