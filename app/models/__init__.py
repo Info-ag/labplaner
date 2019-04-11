e9d2e6f516d3f97abc3214da09fda222fc314f7a
Jonas Drotleff <jonas.drotleff@gmail.com>
2019-04-11 14:17:54 +0200

    add style guide

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,15 @@
 This package contains all application models and schemes used for
 database access and serialization.
 We use SQLAlchemy and Marshmallow for our models and schemes.
+
+Style guide:
+    - Use plural form for database names (e.g. 'users', 'ags')
+    - For relationships, use database names in alphabetical order,
+    seperated by an underscore (e.g. 'ags_users', 'dates_events')
+    - If possible use `id` for primary key
+    - Use [table_name]_id for foreign keys, where table_name ist the
+    singular form of the corresponding table (e.g. 'user_id', 'ag_id')
+
 """
 from flask_sqlalchemy import SQLAlchemy
 
